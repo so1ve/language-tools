5aa3be7969d64bc31e7186649d0a613a6cb56f82
so1ve <i@mk1.io>
2024-04-08 11:40:35 +0800

    tests

---
+++ test-workspace/tsc/vue3/#4198/main.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import child from './child.vue';
+
+const msg = ref('');
+</script>
+
+<template>
+  <child @vnode-before-mount=""
+    v-bind="{
+      onBark: ($event) => { msg = $event }
+    }"
+  />
+</template>
